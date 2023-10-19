113136c763d6e4dc2a6f10844f302770b4065590
NicholasPilotto <nicholaspilotto@gmail.com>
2023-10-19 21:25:39 +0200

    Marked class with @Component

--- api-gateway/src/main/java/com/nicholaspilotto/apigateway/LogginFilter.java
+++ api-gateway/src/main/java/com/nicholaspilotto/apigateway/LogginFilter.java
@@ -1,18 +1,19 @@
 package com.nicholaspilotto.apigateway;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Component
 public class LogginFilter implements GlobalFilter {
-  private  final Logger logger = LoggerFactory.getLogger(LogginFilter.class);
+  private final Logger logger = LoggerFactory.getLogger(LogginFilter.class);
 
-  @Override
-  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-    logger.info("Request path -> {}", exchange.getRequest().getPath());
+  @Override public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    logger.info("Path of the request received -> {}", exchange.getRequest().getPath());
     return chain.filter(exchange);
   }
 }
