eeea06f7502ff8385e0ae64b580c6ee00a8fdf59
NicholasPilotto <nicholaspilotto@gmail.com>
2023-10-19 21:24:53 +0200

    get method working

--- api-gateway/src/main/java/com/nicholaspilotto/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/nicholaspilotto/apigateway/ApiGatewayConfiguration.java
@@ -1,9 +1,13 @@
 package com.nicholaspilotto.apigateway;
 
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ApiGatewayConfiguration {
@@ -11,8 +15,12 @@ public class ApiGatewayConfiguration {
   public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
     return builder.routes()
       .route(predicateSpec -> {
-        return predicateSpec.path("/test")
-          .uri("https://httpbin.org/ip");
+        return predicateSpec.path("/get")
+          .filters(f -> f
+            .addRequestHeader("MyHeader", "MyUri")
+            .addRequestParameter("MyParam", "MyValue")
+          )
+          .uri("http://httpbin.org:80");
       })
       .build();
   }
