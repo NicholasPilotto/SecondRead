9e2aa81050f3f13b78a3397b0553f55c5f0127a3
NicholasPilotto <nicholaspilotto@gmail.com>
2023-10-22 14:48:08 +0200

    Return json object on test call

--- user-service/src/main/java/com/nicholaspilotto/userservice/controllers/UserController.java
+++ user-service/src/main/java/com/nicholaspilotto/userservice/controllers/UserController.java
@@ -3,10 +3,16 @@ package com.nicholaspilotto.userservice.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class UserController {
   @GetMapping("/user/test")
-  public String UserTest() {
-    return "Test";
+  public Map<String, String> UserTest() {
+    HashMap<String, String> map = new HashMap<>();
+    map.put("Test", "This is a test call.");
+    map.put("Service", "User");
+    return map;
   }
 }
