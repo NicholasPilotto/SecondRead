58ad8b018de823bfc3c3ef508d24ceb8db5344b0
NicholasPilotto <nicholaspilotto@gmail.com>
2023-10-22 14:48:25 +0200

    User service circuit breaker class

--- user-service/src/main/java/com/nicholaspilotto/userservice/controllers/circuitbreaker/CircuitBreakerController.java
+++ user-service/src/main/java/com/nicholaspilotto/userservice/controllers/circuitbreaker/CircuitBreakerController.java
@@ -1,2 +1,24 @@
-package com.nicholaspilotto.userservice.circuitbreaker;public class CircuitBreakerController {
+package com.nicholaspilotto.userservice.controllers.circuitbreaker;
+
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+  private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+  @GetMapping("/sample-api")
+  @Bulkhead(name = "sample-api")
+  public String sampleApi() {
+    logger.info("Sample API call received");
+    ResponseEntity<String> forEntity = new RestTemplate()
+      .getForEntity("http://localhost:8080/some-dummy-url", String.class);
+    return  forEntity.getBody();
+  }
 }
