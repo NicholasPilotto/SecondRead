4716bac48051ff178a1be62e932afc197624484d
NicholasPilotto <nicholaspilotto@gmail.com>
2023-10-22 14:48:59 +0200

    Using Jupiter test

--- user-service/src/test/java/com/nicholaspilotto/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/nicholaspilotto/userservice/UserServiceApplicationTests.java
@@ -1,9 +1,7 @@
 package com.nicholaspilotto.userservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class UserServiceApplicationTests {
 
 	@Test
