c03ab37ec5878d231338c3ad75c8b08698a3a42e
NicholasPilotto <nicholaspilotto@gmail.com>
2023-10-23 21:18:08 +0200

    Create rest template

--- user-service/src/main/java/com/nicholaspilotto/userservice/controllers/UserController.java
+++ user-service/src/main/java/com/nicholaspilotto/userservice/controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.nicholaspilotto.userservice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class UserController {
+  @Autowired
+  private RestTemplate restTemplate;
+  
   @GetMapping("/user/test")
   public Map<String, String> UserTest() {
     HashMap<String, String> map = new HashMap<>();
