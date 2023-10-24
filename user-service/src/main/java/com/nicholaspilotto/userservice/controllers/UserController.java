3217b905bfbdc19abdcef40fd6933fcffc955f6e
NicholasPilotto <nicholaspilotto@gmail.com>
2023-10-24 21:04:09 +0200

    Import userservice class to interact with user data

--- user-service/src/main/java/com/nicholaspilotto/userservice/controllers/UserController.java
+++ user-service/src/main/java/com/nicholaspilotto/userservice/controllers/UserController.java
@@ -1,6 +1,9 @@
 package com.nicholaspilotto.userservice.controllers;
 
+import com.nicholaspilotto.userservice.services.CustomerUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -8,16 +11,29 @@ import org.springframework.web.client.RestTemplate;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents a controller class used to interact
+ * with user data.
+ */
 @RestController
 public class UserController {
   @Autowired
   private RestTemplate restTemplate;
-  
+
+  /**
+   * CustomerUserService class.
+   * It is used to manage user data and
+   * interact with database.
+   */
+  @Autowired
+  private CustomerUserService customerUserService;
+
+  /**
+   * Method used to test current controller.
+   * @return response entity with test data.
+   */
   @GetMapping("/user/test")
-  public Map<String, String> UserTest() {
-    HashMap<String, String> map = new HashMap<>();
-    map.put("Test", "This is a test call.");
-    map.put("Service", "User");
-    return map;
+  public ResponseEntity<String> UserTest() {
+    return new ResponseEntity<>(customerUserService.getUser(), HttpStatus.OK);
   }
 }
