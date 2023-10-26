ab522f80e5a34c69e359d13b35318080a1fe51b3
NicholasPilotto <nicholaspilotto@gmail.com>
2023-10-26 22:04:59 +0200

    User repository

--- user-service/src/main/java/com/nicholaspilotto/userservice/repositories/UserRepository.java
+++ user-service/src/main/java/com/nicholaspilotto/userservice/repositories/UserRepository.java
@@ -3,6 +3,4 @@ package com.nicholaspilotto.userservice.repositories;
 import com.nicholaspilotto.userservice.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-  User findOne(long id);
-}
+public interface UserRepository extends JpaRepository<User, Long> { }
