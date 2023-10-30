03f54fe8b8bdc1f324bce3aef511dd39363a7c85
NicholasPilotto <nicholaspilotto@gmail.com>
2023-10-30 20:49:48 +0100

    Enable jpa repositories

--- user-service/src/main/java/com/nicholaspilotto/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/nicholaspilotto/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.nicholaspilotto.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.nicholaspilotto.userservice.repositories")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
