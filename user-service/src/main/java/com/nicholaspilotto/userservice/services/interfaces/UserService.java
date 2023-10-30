de6278c58f30e07df3bfc3c07cc32fa020135015
NicholasPilotto <nicholaspilotto@gmail.com>
2023-10-30 20:50:47 +0100

    Get the list of all users

--- user-service/src/main/java/com/nicholaspilotto/userservice/services/interfaces/UserService.java
+++ user-service/src/main/java/com/nicholaspilotto/userservice/services/interfaces/UserService.java
@@ -2,9 +2,22 @@ package com.nicholaspilotto.userservice.services.interfaces;
 
 import com.nicholaspilotto.userservice.models.entities.User;
 
+import java.util.List;
+
 /**
  * Represents the interface class used to interact with generic user data.
  */
 public interface UserService {
+  /**
+   * Get the list of all users.
+   * @return List containing all users.
+   */
+  public abstract List<User> getAllUsers();
+
+  /**
+   * Get a specific user by its id.
+   * @param id id of the target user.
+   * @return User with {@code id}.
+   */
   public abstract User getUser(Long id);
 }
