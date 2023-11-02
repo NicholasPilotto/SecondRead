00251a2a314a3275de4aace62d1dc870ce606882
NicholasPilotto <nicholaspilotto@gmail.com>
2023-11-02 21:50:54 +0100

    Get all user with pagination

--- user-service/src/main/java/com/nicholaspilotto/userservice/controllers/UserController.java
+++ user-service/src/main/java/com/nicholaspilotto/userservice/controllers/UserController.java
@@ -7,10 +7,11 @@ import com.nicholaspilotto.userservice.services.CustomerUserService;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,43 +21,69 @@ import java.util.List;
  * with user data.
  */
 @RestController
+@RequestMapping("/user")
 public class UserController {
-  @Autowired
-  private RestTemplate restTemplate;
-
   /**
    * CustomerUserService class.
    * It is used to manage user data and
    * interact with database.
    */
-  @Autowired
-  private CustomerUserService customerUserService;
+  private final CustomerUserService customerUserService;
+
+  /**
+   * ModelMapper utility used to map entities into dto
+   * and vice-versa.
+   */
+  private final ModelMapper mapper;
 
   @Autowired
-  private ModelMapper mapper;
+  public UserController(
+    CustomerUserService customerUserService,
+    ModelMapper mapper
+  ) {
+    this.customerUserService = customerUserService;
+    this.mapper = mapper;
+  }
 
   /**
    * Get the list of all users stored into the database.
+   * @param page number of the desired page. Default value is 0.
+   * @param size number of elements in the page. Default value is 20.
    * @return List of users.
    */
-  @GetMapping("/user")
-  public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
-    List<User> users = customerUserService.getAllUsers();
+  @GetMapping()
+  public ResponseEntity<?> getAllUsers(
+    @RequestParam(defaultValue = "0") int page,
+    @RequestParam(defaultValue = "20") int size
+  ) {
+    Pageable pageable = PageRequest.of(page, size);
+
+    List<User> users = customerUserService.getAllUsers(pageable).getContent();
     List<UserResponseDTO> response = Arrays.stream(mapper.map(users, UserResponseDTO[].class)).toList();
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
+
+  /**
+   * Get the number of user store into the database.
+   * @return Number of user store into the database.
+   */
+ @GetMapping("/count")
+ public ResponseEntity<Long> count() {
+    Long count = customerUserService.count();
+    return new ResponseEntity<>(count, HttpStatus.OK);
+ }
 
   /**
    * Method used to get user by ig.
    * @param id id of the user we are looking for.
    * @return response entity representing user object if user is found, {@code NOT FOUND} otherwise.
    */
-  @GetMapping("/user/{id}")
-  public ResponseEntity<User> getById(@PathVariable Long id) {
-    User user = customerUserService.getUser(id);
+  @GetMapping("/{id}")
+  public ResponseEntity<?> getById(@PathVariable Long id) {
+    User user = customerUserService.getUser(id).orElse(null);
 
     if (user == null) {
-      return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+      return new ResponseEntity<>("User with provided id does not exist.", HttpStatus.NOT_FOUND);
     }
 
     return new ResponseEntity<>(user, HttpStatus.OK);
@@ -67,7 +94,7 @@ public class UserController {
    * @param payload user data to store into the database.
    * @return Created user object.
    */
-  @PostMapping("/user")
+  @PostMapping()
   public ResponseEntity<UserResponseDTO> create(@Valid @RequestBody UserCreationDTO payload) {
     User newUser = mapper.map(payload, User.class);
     newUser = customerUserService.createUser(newUser);
