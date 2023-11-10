0d17e76f78bb0d06e0bf548c447d6ff34c44843f
NicholasPilotto <nicholaspilotto@gmail.com>
2023-11-10 22:31:03 +0100

    Keep working on jwt auth filter

--- user-service/src/main/java/com/nicholaspilotto/userservice/configuration/authentication/JwtAuthenticationFilter.java
+++ user-service/src/main/java/com/nicholaspilotto/userservice/configuration/authentication/JwtAuthenticationFilter.java
@@ -1,4 +1,62 @@
 package com.nicholaspilotto.userservice.configuration.authentication;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import reactor.util.annotation.NonNull;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+  private final JwtService jwtService;
+  private final UserDetailsService userDetailsService;
+
+  @Autowired
+  public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+    this.jwtService = jwtService;
+    this.userDetailsService = userDetailsService;
+  }
+
+  /**
+   * Manages the filters chain, decoding JWT from the request.
+   *
+   * @param request intercepted incoming request.
+   * @param response intercepted out coming response.
+   * @param filterChain list of filters we need to execute.
+   * @throws ServletException exception.
+   * @throws IOException exception.
+   */
+  @Override
+  protected void doFilterInternal(
+    @NonNull HttpServletRequest request,
+    @NonNull HttpServletResponse response,
+    @NonNull FilterChain filterChain
+  ) throws ServletException, IOException {
+    final String authHeader = request.getHeader("Authorization");
+    final String jwt;
+    final String bearerSuffix = "Bearer ";
+
+    final String userEmail;
+
+    if (authHeader == null || authHeader.startsWith(bearerSuffix)) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    // get jwt from Bearer authorization header
+    jwt = authHeader.substring(bearerSuffix.length());
+    userEmail = jwtService.extractUserEmail(jwt);
+
+    if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+    }
+  }
 }
