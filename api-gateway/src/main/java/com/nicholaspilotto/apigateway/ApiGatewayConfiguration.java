e7dd75d773c4f6ef7e769f0345114d79f6c989a2
NicholasPilotto <nicholaspilotto@gmail.com>
2023-11-15 21:17:20 +0100

    Restoring api-gateway

--- api-gateway/src/main/java/com/nicholaspilotto/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/nicholaspilotto/apigateway/ApiGatewayConfiguration.java
@@ -11,22 +11,6 @@ public class ApiGatewayConfiguration {
   public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
     return builder.routes()
       .route(predicateSpec -> {
-        return predicateSpec.path("/get")
-          .filters(f -> f
-            .addRequestHeader("MyHeader", "MyUri")
-            .addRequestParameter("MyParam", "MyValue")
-          )
-          .uri("http://httpbin.org/get");
-      })
-      .route(predicateSpec -> {
-        return predicateSpec.path("/post")
-          .filters(f -> f
-            .addRequestHeader("MyHeader", "MyUri")
-            .addRequestParameter("MyParam", "MyValue")
-          )
-          .uri("http://httpbin.org/post");
-      })
-      .route(predicateSpec -> {
         return predicateSpec.path("/user/**")
           .uri("lb://user-service");
       })
