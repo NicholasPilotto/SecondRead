d4509229f40ea50826b798025c4719e7682d1953
NicholasPilotto <nicholaspilotto@gmail.com>
2023-11-23 21:29:30 +0100

    Documentation code

--- user-service/src/main/java/com/nicholaspilotto/userservice/repositories/UserRepository.java
+++ user-service/src/main/java/com/nicholaspilotto/userservice/repositories/UserRepository.java
@@ -5,7 +5,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * Represents the repository class for the {@link com.nicholaspilotto.userservice.models.entities.User }
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
+  /**
+   * Find {@link com.nicholaspilotto.userservice.models.entities.User} with a particular
+   * {@code email}.
+   *
+   * @param email Email of the desired {@code User}.
+   * @return {@code Optiona<User>} with particular {@code email}.
+   */
   Optional<User> findByEmail(String email);
+
+  /**
+   * Find {@link User} with particular {@code email} and {@code password}.
+   *
+   * @param email Email of the desired {@code User}.
+   * @param password Password of the desired {@code User}.
+   * @return {@code Optional<User>} with {@code email} and {@code password}.
+   */
   Optional<User> findByEmailAndPassword(String email, String password);
 }
