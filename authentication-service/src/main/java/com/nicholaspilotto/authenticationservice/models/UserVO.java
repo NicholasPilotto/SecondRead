ceae085f69e5d894a06596e2dd34c5d47daf2389
NicholasPilotto <nicholaspilotto@gmail.com>
2023-11-30 22:02:23 +0100

    Match response user

--- authentication-service/src/main/java/com/nicholaspilotto/authenticationservice/models/UserVO.java
+++ authentication-service/src/main/java/com/nicholaspilotto/authenticationservice/models/UserVO.java
@@ -1,5 +1,6 @@
 package com.nicholaspilotto.authenticationservice.models;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Objects;
 
@@ -10,14 +11,32 @@ public class UserVO {
   private Date birthDate;
   private String phoneNumber;
   private String email;
-  private String password;
   private Role role;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
 
-  public UserVO(Long id, String email, String password, Role role) {
+  public UserVO() { }
+
+  public UserVO(
+    Long id,
+    String firstName,
+    String lastName,
+    Date birthDate,
+    String phoneNumber,
+    String email,
+    Role role,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt
+  ) {
     this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthDate = birthDate;
+    this.phoneNumber = phoneNumber;
     this.email = email;
-    this.password = password;
     this.role = role;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
   }
 
   public Long getId() {
@@ -28,6 +47,38 @@ public class UserVO {
     this.id = id;
   }
 
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Date getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
   public String getEmail() {
     return email;
   }
@@ -36,20 +87,28 @@ public class UserVO {
     this.email = email;
   }
 
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
   public Role getRole() {
     return role;
   }
 
   public void setRole(Role role) {
     this.role = role;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(LocalDateTime updatedAt) {
+    this.updatedAt = updatedAt;
   }
 
   @Override
@@ -62,23 +121,37 @@ public class UserVO {
     }
     UserVO userVO = (UserVO) o;
     return Objects.equals(id, userVO.id) && Objects.equals(
+      firstName,
+      userVO.firstName
+    ) && Objects.equals(lastName, userVO.lastName) && Objects.equals(
+      birthDate,
+      userVO.birthDate
+    ) && Objects.equals(phoneNumber, userVO.phoneNumber) && Objects.equals(
       email,
       userVO.email
-    ) && Objects.equals(password, userVO.password) && Objects.equals(role, userVO.role);
+    ) && role == userVO.role && Objects.equals(createdAt, userVO.createdAt) && Objects.equals(
+      updatedAt,
+      userVO.updatedAt
+    );
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, email, password, role);
+    return Objects.hash(id, firstName, lastName, birthDate, phoneNumber, email, role, createdAt, updatedAt);
   }
 
   @Override
   public String toString() {
-    return "UserVO{" +
+    return "UserResponseDTO{" +
       "id=" + id +
+      ", firstName='" + firstName + '\'' +
+      ", lastName='" + lastName + '\'' +
+      ", birthDate=" + birthDate +
+      ", phoneNumber='" + phoneNumber + '\'' +
       ", email='" + email + '\'' +
-      ", password='" + password + '\'' +
-      ", role='" + role + '\'' +
+      ", role=" + role +
+      ", createdAt=" + createdAt +
+      ", updatedAt=" + updatedAt +
       '}';
   }
 }
