44247d84f5677794ed3033f88568bf3e5fa83a29
NicholasPilotto <nicholaspilotto@gmail.com>
2023-11-30 22:01:39 +0100

    Create user model

--- authentication-service/src/main/java/com/nicholaspilotto/authenticationservice/models/RegisterRequest.java
+++ authentication-service/src/main/java/com/nicholaspilotto/authenticationservice/models/RegisterRequest.java
@@ -1,16 +1,32 @@
 package com.nicholaspilotto.authenticationservice.models;
 
+import java.util.Date;
+
 public class RegisterRequest {
   private String firstName;
   private String lastName;
-  private String Email;
+  private String email;
+  private Date birthDate;
+  private String phoneNumber;
   private String password;
+  private Role role;
 
-  public RegisterRequest(String firstName, String lastName, String email, String password) {
+  public RegisterRequest(
+    String firstName,
+    String lastName,
+    String email,
+    Date birthDate,
+    String phoneNumber,
+    String password,
+    Role role
+  ) {
     this.firstName = firstName;
     this.lastName = lastName;
-    Email = email;
+    this.email = email;
+    this.birthDate = birthDate;
+    this.phoneNumber = phoneNumber;
     this.password = password;
+    this.role = role;
   }
 
   public String getFirstName() {
@@ -30,11 +46,27 @@ public class RegisterRequest {
   }
 
   public String getEmail() {
-    return Email;
+    return email;
   }
 
   public void setEmail(String email) {
-    Email = email;
+    email = email;
+  }
+
+  public Date getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
   }
 
   public String getPassword() {
@@ -45,13 +77,24 @@ public class RegisterRequest {
     this.password = password;
   }
 
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
+
   @Override
   public String toString() {
     return "RegisterRequest{" +
       "firstName='" + firstName + '\'' +
       ", lastName='" + lastName + '\'' +
-      ", Email='" + Email + '\'' +
+      ", email='" + email + '\'' +
+      ", birthDate=" + birthDate +
+      ", phoneNumber='" + phoneNumber + '\'' +
       ", password='" + password + '\'' +
+      ", role=" + role +
       '}';
   }
 }
