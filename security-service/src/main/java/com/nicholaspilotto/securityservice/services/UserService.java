2b0c38914a399a2d700330403910ba9dfc2abf08
NicholasPilotto <nicholaspilotto@gmail.com>
2023-12-20 20:51:34 +0100

    Removed security-service

--- security-service/src/main/java/com/nicholaspilotto/securityservice/services/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.nicholaspilotto.securityservice.services;
-
-@FeignClient(name = "user-service")
-public interface UserService {
-  @GetMapping("user-service/user/email/{email}")
-  public User findByEmail(@PathVariable String email);
-}
