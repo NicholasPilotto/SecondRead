785b880a4689547cd04c8d35b8671a23ad03d82c
NicholasPilotto <nicholaspilotto@gmail.com>
2023-12-20 21:31:34 +0100

    Login request model

---
+++ authentication-service/src/main/java/com/nicholaspilotto/authenticationservice/models/LoginRequest.java
@@ -0,0 +1,43 @@
+package com.nicholaspilotto.authenticationservice.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class LoginRequest {
+  @Email
+  private String email;
+
+  @NotNull
+  @Size(min = 32, max = 72)
+  private String password;
+
+  public LoginRequest(String email, String password) {
+    this.email = email;
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String toString() {
+    return "LoginRequest{" +
+      "email='" + email + '\'' +
+      ", password='" + password + '\'' +
+      '}';
+  }
+}
