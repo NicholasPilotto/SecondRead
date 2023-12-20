e0951e6b631a53380dfe6b1a0bddd679a310df31
NicholasPilotto <nicholaspilotto@gmail.com>
2023-12-20 21:31:59 +0100

    AdminRole annotation

---
+++ user-service/src/main/java/com/nicholaspilotto/userservice/annotations/RoleAdmin.java
@@ -0,0 +1,11 @@
+package com.nicholaspilotto.userservice.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target({ ElementType.METHOD, ElementType.TYPE })
+public @interface RoleAdmin { }
