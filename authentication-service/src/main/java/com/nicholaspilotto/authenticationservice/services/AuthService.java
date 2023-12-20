e35fa3b2ee5c64760bb4ca500a40c1df3e8812c1
NicholasPilotto <nicholaspilotto@gmail.com>
2023-12-20 21:30:16 +0100

    Login method

--- authentication-service/src/main/java/com/nicholaspilotto/authenticationservice/services/AuthService.java
+++ authentication-service/src/main/java/com/nicholaspilotto/authenticationservice/services/AuthService.java
@@ -1,6 +1,7 @@
 package com.nicholaspilotto.authenticationservice.services;
 
 import com.nicholaspilotto.authenticationservice.models.AuthenticationResponse;
+import com.nicholaspilotto.authenticationservice.models.LoginRequest;
 import com.nicholaspilotto.authenticationservice.models.RegisterRequest;
 import com.nicholaspilotto.authenticationservice.models.UserVO;
 import org.mindrot.jbcrypt.BCrypt;
@@ -21,6 +22,19 @@ public class AuthService {
 
   public AuthenticationResponse register(RegisterRequest payload) {
     UserVO user = restTemplate.postForObject("http://user-service/user", payload, UserVO.class);
+
+    if (user == null) {
+      return null;
+    }
+
+    String accessToken = jwtService.generateToken(user.getId().toString(), user.getRole(), "ACCESS");
+    String refreshToken = jwtService.generateToken(user.getId().toString(), user.getRole(), "REFRESH");
+
+    return new AuthenticationResponse(accessToken, refreshToken);
+  }
+
+  public AuthenticationResponse login(LoginRequest payload) {
+    UserVO user = restTemplate.postForObject("http://user-service/user/login", payload, UserVO.class);
 
     if (user == null) {
       return null;
