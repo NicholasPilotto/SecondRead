aee4fd22a0f97d057cc142f4a5142fe0bbf1a241
NicholasPilotto <nicholaspilotto@gmail.com>
2023-12-20 21:32:35 +0100

    Constants

---
+++ user-service/src/main/java/com/nicholaspilotto/userservice/constants/UserRolesConstants.java
@@ -0,0 +1,7 @@
+package com.nicholaspilotto.userservice.constants;
+
+import com.nicholaspilotto.userservice.models.entities.Role;
+
+public class UserRolesConstants {
+  public static final String HAS_ROLE_ADMIN = "hasAuthority('ADMIN')";
+}
