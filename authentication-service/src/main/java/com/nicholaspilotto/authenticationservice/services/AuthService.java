2c0b9af35bf7c28c261a23f6a72f0a2d3d860a5d
NicholasPilotto <nicholaspilotto@gmail.com>
2023-12-23 19:16:21 +0100

    get user by id + demand jwts building to controller

--- authentication-service/src/main/java/com/nicholaspilotto/authenticationservice/services/AuthService.java
+++ authentication-service/src/main/java/com/nicholaspilotto/authenticationservice/services/AuthService.java
@@ -4,6 +4,7 @@ import com.nicholaspilotto.authenticationservice.models.AuthenticationResponse;
 import com.nicholaspilotto.authenticationservice.models.LoginRequest;
 import com.nicholaspilotto.authenticationservice.models.RegisterRequest;
 import com.nicholaspilotto.authenticationservice.models.UserVO;
+import org.apache.catalina.User;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,37 +13,21 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class AuthService {
   private final RestTemplate restTemplate;
-  private final JwtService jwtService;
 
   @Autowired
   public AuthService(RestTemplate restTemplate, JwtService jwtService) {
     this.restTemplate = restTemplate;
-    this.jwtService = jwtService;
   }
 
-  public AuthenticationResponse register(RegisterRequest payload) {
-    UserVO user = restTemplate.postForObject("http://user-service/user", payload, UserVO.class);
-
-    if (user == null) {
-      return null;
-    }
-
-    String accessToken = jwtService.generateToken(user.getId().toString(), user.getRole(), "ACCESS");
-    String refreshToken = jwtService.generateToken(user.getId().toString(), user.getRole(), "REFRESH");
-
-    return new AuthenticationResponse(accessToken, refreshToken);
+  public UserVO register(RegisterRequest payload) {
+    return restTemplate.postForObject("http://user-service/user", payload, UserVO.class);
   }
 
-  public AuthenticationResponse login(LoginRequest payload) {
-    UserVO user = restTemplate.postForObject("http://user-service/user/login", payload, UserVO.class);
+  public UserVO login(LoginRequest payload) {
+    return restTemplate.postForObject("http://user-service/user/login", payload, UserVO.class);
+  }
 
-    if (user == null) {
-      return null;
-    }
-
-    String accessToken = jwtService.generateToken(user.getId().toString(), user.getRole(), "ACCESS");
-    String refreshToken = jwtService.generateToken(user.getId().toString(), user.getRole(), "REFRESH");
-
-    return new AuthenticationResponse(accessToken, refreshToken);
+  public  UserVO getUserById(Long id) {
+    return restTemplate.getForObject("http://user-service/user/{id}", UserVO.class, id);
   }
 }
