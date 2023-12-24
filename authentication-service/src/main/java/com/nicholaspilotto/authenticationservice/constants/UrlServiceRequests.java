0f8d34b890509b63cb7a88e2b3c59122630597ba
NicholasPilotto <nicholaspilotto@gmail.com>
2023-12-24 15:15:52 +0100

    User service url constant

---
+++ authentication-service/src/main/java/com/nicholaspilotto/authenticationservice/constants/UrlServiceRequests.java
@@ -0,0 +1,5 @@
+package com.nicholaspilotto.authenticationservice.constants;
+
+public final class UrlServiceRequests {
+  public static final String USER_SERVICE_USER = "http://user-service/user/";
+}
