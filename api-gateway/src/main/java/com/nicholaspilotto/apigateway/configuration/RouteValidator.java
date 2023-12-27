0b91ac43bbe716b551eeb8d385e2ab23552bdfa1
NicholasPilotto <nicholaspilotto@gmail.com>
2023-12-27 22:00:56 +0100

    Code documentation

--- api-gateway/src/main/java/com/nicholaspilotto/apigateway/configuration/RouteValidator.java
+++ api-gateway/src/main/java/com/nicholaspilotto/apigateway/configuration/RouteValidator.java
@@ -3,17 +3,33 @@ package com.nicholaspilotto.apigateway.configuration;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+/**
+ * Represents the validator class for the open route.
+ */
 @Service
 public class RouteValidator {
-  public static final List<String> openEndpoints = List.of(
+  private static final List<String> authenticationOpenEndpoints = List.of(
     "auth/register",
-    "auth/login",
-    "auth/whoami"
+    "auth/login"
   );
 
+  /**
+   * Contains all routes that do not need token authentication.
+   */
+  public static final List<String> openEndpoints = Stream.of(authenticationOpenEndpoints)
+    .flatMap(Collection::stream)
+    .collect(Collectors.toList());
+
+  /**
+   * Check if a request is an open URL or not.
+   */
   public Predicate<ServerHttpRequest> isSecured = request ->
     openEndpoints.stream()
                  .noneMatch(uri -> request.getURI().getPath().contains(uri));
