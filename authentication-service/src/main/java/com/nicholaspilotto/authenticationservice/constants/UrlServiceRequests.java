7ce6ec551844dc0a1a2158665de666e02a0b7b09
NicholasPilotto <nicholaspilotto@gmail.com>
2023-12-27 21:59:45 +0100

    Moved class inside ProjectConstants

--- authentication-service/src/main/java/com/nicholaspilotto/authenticationservice/constants/UrlServiceRequests.java
+++
@@ -1,5 +0,0 @@
-package com.nicholaspilotto.authenticationservice.constants;
-
-public final class UrlServiceRequests {
-  public static final String USER_SERVICE_USER = "http://user-service/user/";
-}
