92ef0b60dae771acea2b5bad39529f8ce888fd1c
NicholasPilotto <nicholaspilotto@gmail.com>
2023-12-27 21:21:15 +0100

    Code documentation

--- user-service/src/main/java/com/nicholaspilotto/userservice/constants/ProjectConstants.java
+++ user-service/src/main/java/com/nicholaspilotto/userservice/constants/ProjectConstants.java
@@ -1,7 +1,30 @@
 package com.nicholaspilotto.userservice.constants;
 
-import com.nicholaspilotto.userservice.models.entities.Role;
+/**
+ * Contains all constant values used in all project.
+ */
+public class ProjectConstants {
+  /**
+   * Contains constants that represent {@code HTTP} headers.
+   */
+  public static class HTTP_HEADERS {
+    /**
+     * {@code HTTP} authorization header.
+     */
+    public static final String AUTHORIZATION = "Authorization";
+    /**
+     * {@code Bearer} token prefix.
+     */
+    public static final String BEARER = "Bearer ";
+  }
 
-public class UserRolesConstants {
-  public static final String HAS_ROLE_ADMIN = "hasAuthority('ADMIN')";
+  /**
+   * Contains all constants used to get values from {@code JWT} token claims.
+   */
+  public static class CLAIMS {
+    /**
+     * {@code role} claims.
+     */
+    public static final String ROLE = "role";
+  }
 }
