f910ad153493d4ba1f367174293e901e1f69a46b
NicholasPilotto <nicholaspilotto@gmail.com>
2023-12-29 21:08:37 +0100

    Documentation code

--- api-gateway/src/main/java/com/nicholaspilotto/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/nicholaspilotto/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.nicholaspilotto.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application entry method.
+ */
 @SpringBootApplication
 public class ApiGatewayApplication {
 
