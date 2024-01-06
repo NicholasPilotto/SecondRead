306a9f63e62ac2b954e368c42fc115d939b22151
NicholasPilotto <nicholaspilotto@gmail.com>
2024-01-06 22:49:57 +0100

    Start working on book service

---
+++ book-service/src/main/java/com/nicholaspilotto/bookservice/services/BookServiceImplementation.java
@@ -0,0 +1,11 @@
+package com.nicholaspilotto.bookservice.services;
+
+import com.nicholaspilotto.bookservice.services.interfaces.BookService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Implementation service class of {@link BookService}.
+ */
+@Service
+public class BookServiceImplementation implements BookService {
+}
---
+++ book-service/src/main/java/com/nicholaspilotto/bookservice/services/interfaces/BookService.java
@@ -0,0 +1,7 @@
+package com.nicholaspilotto.bookservice.services.interfaces;
+
+/**
+ * Represents the service layer for {@link com.nicholaspilotto.bookservice.models.entities.Book} entity.
+ */
+public interface BookService {
+}
