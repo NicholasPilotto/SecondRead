7c3297ff9c99a98ddad90c2402a95e6d195f6b1d
NicholasPilotto <nicholaspilotto@gmail.com>
2024-01-06 22:49:11 +0100

    Start working on book controller

---
+++ book-service/src/main/java/com/nicholaspilotto/bookservice/controllers/BookController.java
@@ -0,0 +1,13 @@
+package com.nicholaspilotto.bookservice.controllers;
+
+import com.nicholaspilotto.bookservice.models.entities.Book;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller class for {@link Book} entity.
+ */
+@RestController
+@RequestMapping("/book")
+public class BookController {
+}
