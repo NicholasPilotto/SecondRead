63e831358022293d780a13627cccc0fcc8db4e7c
NicholasPilotto <nicholaspilotto@gmail.com>
2024-01-08 21:24:40 +0100

    Get all books

--- book-service/src/main/java/com/nicholaspilotto/bookservice/services/BookServiceImplementation.java
+++ book-service/src/main/java/com/nicholaspilotto/bookservice/services/BookServiceImplementation.java
@@ -1,6 +1,11 @@
 package com.nicholaspilotto.bookservice.services;
 
+import com.nicholaspilotto.bookservice.models.entities.Book;
+import com.nicholaspilotto.bookservice.repositories.BookRepository;
 import com.nicholaspilotto.bookservice.services.interfaces.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,4 +13,20 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BookServiceImplementation implements BookService {
+  private final BookRepository bookRepository;
+
+  /**
+   * Creates a new instance of {@link BookServiceImplementation} object.
+   *
+   * @param bookRepository {@link BookRepository} injected reference.
+   */
+  @Autowired
+  public BookServiceImplementation(BookRepository bookRepository) {
+    this.bookRepository = bookRepository;
+  }
+
+  @Override
+  public Page<Book> getAllBooks(Pageable pageable) {
+    return bookRepository.findAll(pageable);
+  }
 }
--- book-service/src/main/java/com/nicholaspilotto/bookservice/services/interfaces/BookService.java
+++ book-service/src/main/java/com/nicholaspilotto/bookservice/services/interfaces/BookService.java
@@ -1,7 +1,19 @@
 package com.nicholaspilotto.bookservice.services.interfaces;
 
+import com.nicholaspilotto.bookservice.models.entities.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 /**
  * Represents the service layer for {@link com.nicholaspilotto.bookservice.models.entities.Book} entity.
  */
 public interface BookService {
+  /**
+   * Get the list of all {@link Book} stored.
+   *
+   * @param pageable {@link Pageable} object to paginate current result.
+   *
+   * @return The list of all {@link Book} stored, eventually paginated.
+   */
+  public abstract Page<Book> getAllBooks(Pageable pageable);
 }
