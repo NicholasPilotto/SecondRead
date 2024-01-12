16eead7f9fa81e08c402fc964a43612f92880757
NicholasPilotto <nicholaspilotto@gmail.com>
2024-01-12 22:21:38 +0100

    Query operators enum

---
+++ user-service/src/main/java/com/nicholaspilotto/userservice/filters/specifications/enums/QueryOperator.java
@@ -0,0 +1,13 @@
+package com.nicholaspilotto.userservice.filters.specifications.enums;
+
+/**
+ * Represents the operator that a query can have.
+ */
+public enum QueryOperator {
+  GREATER_THAN,
+  LESS_THAN,
+  EQUALS,
+  LIKE,
+  NOT_EQ,
+  IN
+}
