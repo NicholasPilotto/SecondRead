44af173ad60d2999e824b6a2a7c5eb440883e46e
NicholasPilotto <nicholaspilotto@gmail.com>
2024-01-13 19:20:56 +0100

    Using specifications on queries

--- user-service/src/main/java/com/nicholaspilotto/userservice/services/CustomerUserService.java
+++ user-service/src/main/java/com/nicholaspilotto/userservice/services/CustomerUserService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 
@@ -20,8 +21,8 @@ public class CustomerUserService implements UserService {
   private UserRepository userRepository;
 
   @Override
-  public Page<User> getAllUsers(Pageable pageable) {
-    return userRepository.findAll(pageable);
+  public Page<User> getAllUsers(Pageable pageable, Specification<User> specification) {
+    return userRepository.findAll(specification, pageable);
   }
 
   @Override
