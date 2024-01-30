5076a2805bcab02199e8da37be66f8624fc47485
NicholasPilotto <nicholaspilotto@gmail.com>
2024-01-30 22:02:27 +0100

    Book genres

---
+++ book-service/src/main/java/com/nicholaspilotto/bookservice/models/Enums/BookGenre.java
@@ -0,0 +1,103 @@
+package com.nicholaspilotto.bookservice.models.Enums;
+
+/**
+ * Represents all genre that a {@link com.nicholaspilotto.bookservice.models.entities.Book} can be.
+ */
+public enum BookGenre {
+  /**
+   * Fantasy book.
+   */
+  Fantasy,
+  /**
+   * Adventure book.
+   */
+  Adventure,
+  /**
+   * Romance book.
+   */
+  Romance,
+  /**
+   * Contemporary book.
+   */
+  Contemporary,
+  /**
+   * Dystopian book.
+   */
+  Dystopian,
+  /**
+   * Mystery book.
+   */
+  Mystery,
+  /**
+   * Horror book.
+   */
+  Horror,
+  /**
+   * Thriller book.
+   */
+  Thriller,
+  /**
+   * Paranormal book.
+   */
+  Paranormal,
+  /**
+   * Historical fiction book.
+   */
+  HistoricalFiction,
+  /**
+   * Science fiction book.
+   */
+  ScienceFiction,
+  /**
+   * Children book.
+   */
+  Children,
+  /**
+   * Memoir book.
+   */
+  Memoir,
+  /**
+   * Cookbook book.
+   */
+  Cookbook,
+  /**
+   * Art book.
+   */
+  Art,
+  /**
+   * Self-help book.
+   */
+  SelfHelp,
+  /**
+   * Personal development book.
+   */
+  PersonalDevelopment,
+  /**
+   * Motivational book.
+   */
+  Motivational,
+  /**
+   * Health book.
+   */
+  Health,
+  /**
+   * History book.
+   */
+  History,
+  /**
+   * Travel book.
+   */
+  Travel,
+  /**
+   * Guide book.
+   */
+  Guide,
+  /**
+   * Families and relationships book.
+   */
+  FamiliesAndRelationships,
+  /**
+   * Humor book.
+   */
+  Humor,
+}
