8fa2ba94ab7ed53219e76eb2b7ac8ed8fcabf384
NicholasPilotto <nicholaspilotto@gmail.com>
2024-02-08 22:14:16 +0100

    Constants

---
+++ book-service/src/main/java/com/nicholaspilotto/bookservice/constants/ProjectConstants.java
@@ -0,0 +1,40 @@
+package com.nicholaspilotto.bookservice.constants;
+
+/**
+ * Contains all constant values used in all project.
+ */
+public class ProjectConstants {
+  /**
+   * Contains constants that represent {@code HTTP} headers.
+   */
+  public static class HttpHeaders {
+    /**
+     * {@code HTTP} authorization header.
+     */
+    public static final String AUTHORIZATION = "Authorization";
+    /**
+     * {@code Bearer} token prefix.
+     */
+    public static final String BEARER = "Bearer ";
+  }
+
+  /**
+   * Contains all constants used to get values from {@code JWT} token claims.
+   */
+  public static class Claims {
+    /**
+     * {@code role} claims.
+     */
+    public static final String ROLE = "role";
+  }
+
+  /**
+   * Project {@link String} constants.
+   */
+  public static class StringConstants {
+    /**
+     * Represents an empty {@link String}.
+     */
+    public static final String EMPTY = "";
+  }
+}
