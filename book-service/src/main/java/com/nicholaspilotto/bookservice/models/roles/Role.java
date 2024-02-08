e6a098a36fe39ca323cd283f2af88bbb27fc62e0
NicholasPilotto <nicholaspilotto@gmail.com>
2024-02-08 22:15:10 +0100

    Role model

---
+++ book-service/src/main/java/com/nicholaspilotto/bookservice/models/roles/Role.java
@@ -0,0 +1,36 @@
+package com.nicholaspilotto.bookservice.models.roles;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+/**
+ * Represents the role that a User can have.
+ */
+@JsonFormat(shape = JsonFormat.Shape.NUMBER)
+public enum Role {
+  /**
+   * The user is an admin of the system.
+   */
+  ADMIN,
+  /**
+   * The user is customer.
+   */
+  CUSTOMER,
+  /**
+   * The user an owner.
+   */
+  OWNER,
+  /**
+   * The user an employee.
+   */
+  EMPLOYEE,
+  ;
+
+  @Override
+  public String toString() {
+    return this.name();
+  }
+
+  public String ordinalString() {
+    return "%s".formatted(this.ordinal());
+  }
+}
