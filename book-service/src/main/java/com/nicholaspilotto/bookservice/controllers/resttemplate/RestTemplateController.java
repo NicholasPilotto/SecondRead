6b17b4e63384b675dff399a6ce3354ca612fc643
NicholasPilotto <nicholaspilotto@gmail.com>
2024-02-08 22:14:53 +0100

    RestTemplateBuilderConfiguration bean builder

---
+++ book-service/src/main/java/com/nicholaspilotto/bookservice/controllers/resttemplate/RestTemplateController.java
@@ -0,0 +1,24 @@
+package com.nicholaspilotto.bookservice.controllers.resttemplate;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Configuration class for {@link RestTemplate}.
+ */
+@Configuration(proxyBeanMethods = false)
+public class RestTemplateController {
+  /**
+   * Build a new instance of {@link RestTemplate} object.
+   *
+   * @param builder {@link RestTemplateBuilder} object reference.
+   *
+   * @return new instance of {@link RestTemplate}.
+   */
+  @Bean
+  RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.build();
+  }
+}
