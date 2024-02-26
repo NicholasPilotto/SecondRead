ac84129ceb505377fbcb5a4c543c7e5da9c59f6e
NicholasPilotto <nicholaspilotto@gmail.com>
2024-02-26 21:02:31 +0100

    Return created user data

--- webapp/secondread/src/services/api.ts
+++ webapp/secondread/src/services/api.ts
@@ -1,12 +1,13 @@
 import { User } from "@/models/user.model";
 import axios from 'axios';
 
-export async function createUser(user: User): Promise<User> {
+export async function createUser(user: User): Promise<User | null> {
   try {
     const response = await axios.post('', user.toPayload());
     console.log(response);
-    return new User().deserialize(response);
+    return response.data;
   } catch (err) {
     console.log(err);
+    return null;
   }
 }
